2219b2ebe3a9df1b5d443abc8301a8d09fb04148
ChandanshiveSupriya <145016796+Eng25-SC@users.noreply.github.com>
2025-07-20 17:45:11 +0530

    Removed unused react-helmet-async and add react-helmet

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,38 +1,34 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-
-import { Helmet, HelmetProvider } from 'react-helmet-async';
-import Footer from '../components/Footer';
-import SearchBar from '../components/SearchBar';
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Helmet } from "react-helmet"; // ✅ Using react-helmet now
+import Footer from "../components/Footer";
+import SearchBar from "../components/SearchBar";
 
 function Layout({
-    children,
-    title = "Forever- Ecommerce app",
-    description = "Mern stack project",
-    keywords = "mern, react, node, mongodb",
-    author = ""
+  children,
+  title = "Forever - Ecommerce App",
+  description = "MERN stack project",
+  keywords = "mern, react, node, mongodb",
+  author = "Supriya"
 }) {
-    return (
-        <HelmetProvider>
-            <div>
-                <Helmet>
-                    <title>{title}</title>
-                    <meta charSet="UTF-8" />
-                    <meta name="description" content={description} />
-                    <meta name="keywords" content={keywords} />
-                    <meta name="author" content={author} />
-                </Helmet>
+  return (
+    <div>
+      <Helmet>
+        <title>{title}</title>
+        <meta charSet="UTF-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+      </Helmet>
 
-                <Navbar />
-                <SearchBar/>
-                <main style={{ minHeight: "70vh" , }} className='my-10'>
-              
-                    {children}
-                </main>
-              <Footer/>
-            </div>
-        </HelmetProvider>
-    );
+      <Navbar />
+      <SearchBar />
+      <main style={{ minHeight: "70vh" }} className="my-10">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default Layout;
